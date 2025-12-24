31331f07152197b7953ca5c430fbd24aff8de212
Adick Rincones <rinconesadick@gmail.com>
2025-12-24 18:30:34 -0400

    fix: ssr importations error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import DepthPhoto from "@/components/DepthPhoto";
+'use client';
+
+import dynamic from "next/dynamic";
+
+const DepthPhotoComponent = dynamic(() => import("@/components/DepthPhoto"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
     <main className="relative w-full h-full overflow-hidden">
-      <DepthPhoto />
+      <DepthPhotoComponent />
     </main>
   );
 }
